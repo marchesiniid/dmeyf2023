ef463579842f99977d9e80c113d7b6e3a4057b2c
marchesiniid <52862950+marchesiniid@users.noreply.github.com>
2023-10-02 14:33:57 -0300

    Update z524_lightgbm_final.r
    changed folder to BO_78, months to train 2019 all

--- src/lightgbm/z524_lightgbm_final.r
+++ src/lightgbm/z524_lightgbm_final.r
@@ -14,12 +14,12 @@ require("lightgbm")
 # defino los parametros de la corrida, en una lista, la variable global  PARAM
 #  muy pronto esto se leera desde un archivo formato .yaml
 PARAM <- list()
-PARAM$experimento <- "KA5240"
+PARAM$experimento <- "KA240_BO_78"
 
 PARAM$input$dataset <- "./datasets/competencia_02.csv.gz"
 
 # meses donde se entrena el modelo
-PARAM$input$training <- c(201901, 201902, 201903, 201904, 201905, 201906, 201907, 201908, 201909,201910, 201911, 201912, 202001, 202002, 202101, 202102, 202103, 202104,202105,202106)
+PARAM$input$training <- c(201901, 201902, 201903, 201904, 201905, 201906, 201907, 201908, 201909,201910, 201911, 201912)
 PARAM$input$future <- c(202107) # meses donde se aplica el modelo
 
 PARAM$finalmodel$semilla <- 500107
