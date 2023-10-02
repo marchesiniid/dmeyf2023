6193816dc13d987d5ec594794d537c716c226c81
marchesiniid <52862950+marchesiniid@users.noreply.github.com>
2023-10-02 14:32:18 -0300

    Update z524_lightgbm_final.r
    iteracion 78, num leaves 1406, ganancia 611590000

--- src/lightgbm/z524_lightgbm_final.r
+++ src/lightgbm/z524_lightgbm_final.r
@@ -24,11 +24,11 @@ PARAM$input$future <- c(202107) # meses donde se aplica el modelo
 
 PARAM$finalmodel$semilla <- 500107
 
-PARAM$finalmodel$num_iterations <- 2726
-PARAM$finalmodel$learning_rate <- 0.043962506
-PARAM$finalmodel$feature_fraction <- 0.72295696
-PARAM$finalmodel$min_data_in_leaf <- 3
-PARAM$finalmodel$num_leaves <- 379
+PARAM$finalmodel$num_iterations <- 2208
+PARAM$finalmodel$learning_rate <- 0.089593826
+PARAM$finalmodel$feature_fraction <- 0.702677631
+PARAM$finalmodel$min_data_in_leaf <- 1406
+PARAM$finalmodel$num_leaves <- 373
 
 
 PARAM$finalmodel$max_bin <- 31
