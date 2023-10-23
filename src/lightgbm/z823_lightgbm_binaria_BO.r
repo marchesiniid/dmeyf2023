8806fbb001774f25a48c9db7caae021f0b4704a4
marchesiniid <52862950+marchesiniid@users.noreply.github.com>
2023-10-23 20:07:50 -0300

    update meses train para exp colaborativo

--- src/lightgbm/z823_lightgbm_binaria_BO.r
+++ src/lightgbm/z823_lightgbm_binaria_BO.r
@@ -40,7 +40,7 @@ PARAM$input$dataset <- "./datasets/competencia_02.csv.gz"
 #  mucha magia emerger de esta eleccion
 PARAM$input$testing <- c(202105)
 PARAM$input$validation <- c(202104)
-PARAM$input$training <- c(201909, 201910, 201911, 201912, 202001, 202002, 202009, 202010, 202011, 202012, 202101, 202102, 202103)
+PARAM$input$training <- c(202010, 202011, 202012, 202101, 202102, 202103)
 
 # un undersampling de 0.1  toma solo el 10% de los CONTINUA
 PARAM$trainingstrategy$undersampling <- 0.85
